5aa235dcf0c5f5285f27bb04aea651739cfe35d1
mlg389 <36056922+mlg389@users.noreply.github.com>
2018-11-15 19:15:46 -0700

    General activity files

---
+++ MainActivity.java
@@ -0,0 +1,15 @@
+package edu.nau.flippyfish;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+// FOR HOMEPAGE (GATEWAY TO - ABOUT PAGE, GAMEPLAY)
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
