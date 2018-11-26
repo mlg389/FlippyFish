c29d9c22ee85ee92c44a7405f7e2af940b4f6275
mlg389 <36056922+mlg389@users.noreply.github.com>
2018-11-26 13:14:04 -0700

    Add files via upload

--- About.java
+++ About.java
@@ -2,12 +2,31 @@ package edu.nau.flippyfish;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.content.Intent;
 
 public class About extends AppCompatActivity {
+
+    private Button BackButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+
+        BackButton = (Button) findViewById(R.id.BackButton);
+
+        BackButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openHome();
+            }
+        });
+    }
+
+    public void openHome() {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }
--- MainActivity.java
+++ MainActivity.java
@@ -2,14 +2,47 @@ package edu.nau.flippyfish;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.content.Intent;
 
 // FOR HOMEPAGE (GATEWAY TO - ABOUT PAGE, GAMEPLAY)
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button button;   // start game
+    private Button button2;  // about
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button = (Button) findViewById(R.id.button);
+        button2 = (Button) findViewById(R.id.button2);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startGame();
+            }
+        });
+
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openAbout();
+            }
+        });
+    }
+
+    public void startGame() {
+        Intent intent = new Intent(this, Gameplay.class);
+        startActivity(intent);
+    }
+
+    public void openAbout() {
+        Intent intent = new Intent(this, About.class);
+        startActivity(intent);
     }
 }
